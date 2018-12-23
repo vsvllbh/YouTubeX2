a7fd5111054c633f2308cbaa016f3cbad0951f5e
naam hai shehenshah <rahanearyan@gmail.com>
2018-12-23 13:12:08 +0530

    1st

--- app/src/main/java/com/example/youtubex/MyVideos.java
+++ app/src/main/java/com/example/youtubex/MyVideos.java
@@ -1,14 +1,151 @@
 package com.example.youtubex;
-
+//import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ListView;
+
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.RetryPolicy;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
+
 public class MyVideos extends AppCompatActivity {
 
+    ListView lvVideo;
+
+    ArrayList<VideoDetails> videoDetailsArrayList;
+
+    CustomListAdapter customListAdapter;
+
+    String searchName;
+
+    String TAG="ChannelActivity";
+
+    String URL="https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UC9CYT9gSNLevX5ey2_6CK0Q&maxResults=25&key=AIzaSyBISSIdnAPG1ZLgJstuWZIc3d1ggj5D8eU";
+
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_my_videos);
+
+        lvVideo=(ListView)findViewById(R.id.videoList);
+
+        videoDetailsArrayList=new ArrayList<>();
+
+        customListAdapter=new CustomListAdapter(MyVideos.this,videoDetailsArrayList);
+
+        showVideo();
+
+
+
     }
+
+
+
+    private void showVideo() {
+
+        RequestQueue requestQueue= Volley.newRequestQueue(getApplicationContext());
+
+        StringRequest stringRequest=new StringRequest(Request.Method.GET, URL, new Response.Listener<String>() {
+
+            @Override
+
+            public void onResponse(String response) {
+
+                try {
+
+                    JSONObject jsonObject=new JSONObject(response);
+
+                    JSONArray jsonArray=jsonObject.getJSONArray("items");
+
+                    for(int i=0;i<jsonArray.length();i++){
+
+                        JSONObject jsonObject1 = jsonArray.getJSONObject(i);
+
+                        JSONObject jsonVideoId=jsonObject1.getJSONObject("id");
+
+                        JSONObject jsonsnippet= jsonObject1.getJSONObject("snippet");
+
+                        JSONObject jsonObjectdefault = jsonsnippet.getJSONObject("thumbnails").getJSONObject("medium");
+
+                        VideoDetails videoDetails=new VideoDetails();
+
+
+
+                        String videoid=jsonVideoId.getString("videoId");
+
+
+
+                        Log.e(TAG," New Video Id" +videoid);
+
+                        videoDetails.setURL(jsonObjectdefault.getString("url"));
+
+                        videoDetails.setVideoName(jsonsnippet.getString("title"));
+
+                        videoDetails.setVideoDesc(jsonsnippet.getString("description"));
+
+                        videoDetails.setVideoId(videoid);
+
+
+
+                        videoDetailsArrayList.add(videoDetails);
+
+                    }
+
+                    lvVideo.setAdapter(customListAdapter);
+
+                    customListAdapter.notifyDataSetChanged();
+
+                } catch (JSONException e) {
+
+                    e.printStackTrace();
+
+                }
+
+
+
+            }
+
+        }, new Response.ErrorListener() {
+
+            @Override
+
+            public void onErrorResponse(VolleyError error) {
+
+                error.printStackTrace();
+                System.out.print("--->");
+
+            }
+
+        });
+
+        int socketTimeout = 30000;
+
+        RetryPolicy policy = new DefaultRetryPolicy(socketTimeout, DefaultRetryPolicy.DEFAULT_MAX_RETRIES, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT);
+
+        stringRequest.setRetryPolicy(policy);
+
+        requestQueue.add(stringRequest);
+
+
+
+    }
+
 }
---
+++ app/src/main/java/com/example/youtubex/VideoDetails.java
@@ -0,0 +1,78 @@
+package com.example.youtubex;
+public class VideoDetails {
+
+    String VideoName;
+
+    String VideoDesc;
+
+    String URL;
+
+    String VideoId;
+
+
+
+
+
+    public void setVideoName(String VideoName){
+
+        this.VideoName=VideoName;
+
+    }
+
+
+
+    public String getVideoName(){
+
+        return VideoName;
+
+    }
+
+
+
+    public void setVideoDesc(String VideoDesc){
+
+        this.VideoDesc=VideoDesc;
+
+    }
+
+
+
+    public String getVideoDesc(){
+
+        return VideoDesc;
+
+    }
+
+
+
+    public void setURL(String URL){
+
+        this.URL=URL;
+
+    }
+
+
+
+    public String getURL(){
+
+        return URL;
+
+    }
+
+
+
+    public void setVideoId(String VideoId){
+
+        this.VideoId=VideoId;
+
+    }
+
+    public String getVideoId(){
+
+        return VideoId;
+
+    }
+
+
+
+}
